6e3bf22b667a081aa91d937344e6e3432f36cfa8
Julio Duarte <julio.duarte@frgnet.com.br>
2024-01-27 21:38:39 -0300

    feat(layout): pre implementacao de botao github

--- src/components/layout/header/index.tsx
+++ src/components/layout/header/index.tsx
@@ -1,27 +1,49 @@
+import { FC, useState } from "react";
+import { Github, Linkedin } from "lucide-react";
+
 import { Button } from "@/components/ui/button";
-import { FC } from "react";
-import { Github } from "lucide-react";
 import { Separator } from "@/components/ui/separator";
+import { twMerge } from "tailwind-merge";
 
-export const Header: FC = () => (
-  <header className="px-6 py-3 flex items-center justify-between border-b col-span-full row-span-1">
-    <h1 className="text-xl font-bold">Button Generator</h1>
+export const Header: FC = () => {
+  const [showText, setShowText] = useState(false);
 
-    <div className="flex items-center gap-3">
-      <span className="text-sm text-muted-foreground">
-        Desenvolvido por Julio Duarte
-      </span>
-      <Separator orientation="vertical" className="h-6" />
+  return (
+    <header className="px-6 py-3 flex items-center justify-between border-b col-span-full row-span-1">
+      <h1 className="text-xl font-bold">Button Generator</h1>
 
-      <a
-        href="https://github.com/iJulioDuarte/button-generator"
-        target="_blank"
-      >
-        <Button variant={"outline"}>
-          <Github className="h-4 w-4 mr-2" />
-          Github
-        </Button>
-      </a>
-    </div>
-  </header>
-);
+      <div className="flex items-center gap-3">
+        <span className="text-sm text-muted-foreground">
+          Desenvolvido por Julio Duarte
+        </span>
+        <Separator orientation="vertical" className="h-6" />
+
+        <a
+          href="https://github.com/iJulioDuarte/button-generator"
+          target="_blank"
+        >
+          <Button variant={"outline"}>
+            <Github className="h-4 w-4 mr-2" />
+            Github
+          </Button>
+        </a>
+        <a href="https://www.linkedin.com/in/ijulio-duarte/" target="_blank">
+          <Button
+            variant={"secondary"}
+            onMouseEnter={() => {
+              setShowText(true);
+            }}
+            onMouseLeave={() => {
+              setShowText(false);
+            }}
+            className={twMerge()}
+          >
+            <Linkedin className="h-4 w-4 mr-2" />
+
+            {showText && "Linkedin"}
+          </Button>
+        </a>
+      </div>
+    </header>
+  );
+};
